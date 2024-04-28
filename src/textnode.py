81bf8851ddf5456f058455ff0103410a585c5d7b
sLmk <Krzysztof@krzysztof.net>
2024-04-28 19:38:17 +0200

    added more TextNode test; fixed TextNode constructor bug

--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,15 @@
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
 class TextNode:
     def __init__(self, text, text_type, url=None) -> None:
         self.text = text
-        self.text_type = text
+        self.text_type = text_type
         self.url = url
 
     def __eq__(self, other) -> bool:
